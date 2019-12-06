c748fbfa79b63db346a4ada76d972c728e58ada5
Wei Zhang <sheepchina@hotmail.com>
2019-12-05 23:44:40 -0600

    Add gulp-autoprefixer

--- ahhSpaceships-incomplete/gulpfile.js
+++ ahhSpaceships-incomplete/gulpfile.js
@@ -1,5 +1,14 @@
 const gulp = require("gulp");
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task("default", function() {
   console.log('Hello world');
 });
+
+exports.default = () => (
+  gulp.src('src/app.css')
+      .pipe(autoprefixer({
+          cascade: false
+      }))
+      .pipe(gulp.dest('dist'))
+);
